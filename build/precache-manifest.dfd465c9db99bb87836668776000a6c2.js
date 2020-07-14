1dd7029c8fe2e1eb2d8350ca76b406f193da65d4
Esther Lim <esther.lim.719@gmail.com>
2020-07-13 22:36:25 -0400

    set up firebase and deploy

---
+++ build/precache-manifest.dfd465c9db99bb87836668776000a6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610296d97efee141cab62fc7c69c1c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "408444a28cd32c2bf0ec",
+    "url": "/static/css/main.8b9e2d94.chunk.css"
+  },
+  {
+    "revision": "9697e8ac1c4dc7dd7d23",
+    "url": "/static/js/2.1f273ebb.chunk.js"
+  },
+  {
+    "revision": "f01cf20f87878beb20bf2f939f8352f1",
+    "url": "/static/js/2.1f273ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408444a28cd32c2bf0ec",
+    "url": "/static/js/main.a582968c.chunk.js"
+  },
+  {
+    "revision": "61a6601a12273ce96c82",
+    "url": "/static/js/runtime-main.ce36dab6.js"
+  }
+]);
